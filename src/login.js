8ef59b42fb6278662d6a98be095c26a30d469129
xXSnehalXx <snehalmachan@gmail.com>
2019-04-23 21:29:31 +0530

    second

---
+++ src/login.js
@@ -0,0 +1,264 @@
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View ,Image ,Platform,Dimensions ,TouchableOpacity ,Button,TextInput ,Animated ,PanResponder,Easing } from 'react-native';
+import MyReactNativeForm from './loginForm.js';
+import CheckBox from 'react-native-checkbox';
+import Swiper from './Components/Swiper';
+
+
+
+
+
+class ViewLogin extends Component{
+
+    constructor(props) {
+      super(props);
+      this.state = {};
+      this.state.time = new Animated.Value(0);
+      const {width,height} = Dimensions.get('window');
+      this.state.left = this.state.time.interpolate({
+         inputRange:[0,100],
+         outputRange:[-100,9],
+         extrapolate:'clamp'
+     });
+        this.state.scale = this.state.time.interpolate({
+           inputRange: [0,40,100],
+           outputRange: [0,0,1],
+           extrapolate:'clamp'
+       });
+
+     Platform.OS=='android'?
+      this.state.top = this.state.time.interpolate({
+         inputRange:[0,100],
+         outputRange:[-100,9],
+         extrapolate:'clamp'
+    })
+    :
+      this.state.top = this.state.time.interpolate({
+         inputRange:[0,100],
+         outputRange:[-100,24],
+         extrapolate:'clamp'
+     });
+
+
+     Platform.OS=='android'?
+     this.state.tC = this.state.time.interpolate({
+        inputRange:[0,40,100,200],
+        outputRange:[height,height,height-80,height-370]
+     })
+    :
+     this.state.tC = this.state.time.interpolate({
+        inputRange:[0,40,100,200],
+        outputRange:[height,height,height-56,height-370]
+    });
+
+    this.at = Platform.OS=='android'? height-80 : height-56;
+
+
+   this.state.panResponder = PanResponder.create({
+     onStartShouldSetPanResponder: () => true,
+     onPanResponderMove: (evt,{vy,dy}) => {
+         if (dy < 0)  {
+             Animated.timing(this.state.time,{
+                 toValue:200,
+                 duration:300,
+                 easing:Easing.inOut(Easing.sin)
+             }).start();
+            } else {
+                Animated.timing(this.state.time,{
+                    toValue:100,
+                    duration:300,
+                    easing:Easing.inOut(Easing.sin)
+                }).start();
+        }
+    },
+     onPanResponderRelease: (evt,{vy,dy}) => {
+         let a = JSON.stringify(this.state.time);
+         let b = JSON.stringify(new Animated.Value(100));
+             if(a === b)
+             {
+                 Animated.timing(this.state.time,{
+                 toValue:200,
+                 duration:300,
+                 easing:Easing.inOut(Easing.sin)
+             }).start();
+         }
+     }
+
+
+ });
+
+}
+
+
+    afterLogInAnim = () => {
+        Animated.timing(this.state.time,{
+            toValue:100,
+            duration:600,
+        }).start();
+    }
+
+
+    render(){
+        return (
+            <Swiper  activeDotColor='#000000' style={styles.wrapper} showsPagination={false} showsButtons={false} loop={false}>
+                <View style= { styles.mainContainer } >
+                  <InfoSectionContainer logAnim = {this.afterLogInAnim}/>
+                  <Animated.View style={[
+                      styles.TopIcons,{
+                        left:this.state.left,
+                        top:this.state.top,
+                        transform:[{scale:this.state.scale}],
+                      }
+                  ]}
+                  >
+                  </Animated.View>
+                  <Animated.View style={[
+                      styles.TopIcons,{
+                        right:this.state.left,
+                        top:this.state.top,
+                        transform:[{scale:this.state.scale}],
+                      }
+                  ]}
+                  >
+                  </Animated.View>
+              <Animated.View style={[
+                  styles.chatBox,{
+                    top:this.state.tC,
+                }
+              ]}
+              {...this.state.panResponder.panHandlers}
+              >
+              </Animated.View>
+          </View>
+
+          <AboutUs/>
+            </Swiper>
+
+
+
+
+
+
+      )
+    }
+
+}
+
+class AboutUs extends Component {
+  render() {
+    return (
+            <View style={styles.AboutUsPage1}>
+
+            </View>
+    );
+  }
+}
+
+const InfoSectionContainer = (props) => {
+    return (
+        <View style={styles.infoSectionContainer}>
+            <LoginContainer logAnim = {props.logAnim}/>
+            <KhaliSpace/>
+        </View>
+    );
+}
+
+
+
+class LoginContainer extends Component{
+
+
+render(){
+    return (
+        <View style={styles.loginContainer}>
+          <ImageContainer/>
+          <MyReactNativeForm logAnim ={this.props.logAnim}/>
+        </View>
+    )
+}
+}
+const KhaliSpace = () => {
+  return (
+    <View style={styles.khaliSpace}>
+
+    </View>
+  );
+}
+const ImageContainer = () => {
+    const ima = require('../images/logo.png');
+  return (
+    <View style={styles.imageContainer}>
+        <Image
+          source={ima}
+          style={{height:200,width:200}}
+          />
+    </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+
+    mainContainer: {
+      flex: 1,
+      backgroundColor: '#37C1A0'
+    },
+    headerContainer:{
+      flex:1,
+      backgroundColor: '#FE616A',
+      alignItems:'center',
+      justifyContent:'center',
+    },
+    headerText:{
+      fontWeight:'300',
+      fontSize:20,
+      color:'white'
+    },
+    infoSectionContainer:{
+      flex:9,
+      backgroundColor:'#F7F0D7'
+    },
+    loginContainer:{
+      flex:3,
+      // backgroundColor:'#383535',
+      margin:4,
+    },
+    khaliSpace:{
+      flex:1,
+      // backgroundColor:'#383535',
+      margin:4
+    },
+    imageContainer:{
+      flex:1,
+      margin:4,
+      // backgroundColor:'#767F58',
+      alignItems:'center',
+      justifyContent:'flex-end'
+  },
+  TopIcons:{
+      height:60,
+      width:60,
+      borderRadius:30,
+      backgroundColor:'#D3D3D3',
+      position:'absolute'
+  },
+  chatBox:{
+      height:300,
+      width:'90%',
+      backgroundColor:'#C56277',
+      position:'absolute',
+      alignSelf:'center',
+      borderRadius:22
+  },
+  AboutUsPage1:{
+    flex:1,
+    borderRadius:25,
+    margin:4,
+    backgroundColor:'#B5F44A',
+
+  },
+});
+
+export default ViewLogin;
---
+++ src/loginForm.js
@@ -0,0 +1,189 @@
+
+import {withFormik,Formik} from 'formik';
+import React, { Component } from 'react';
+import * as Animatable from 'react-native-animatable';
+
+import { View,Text,Button,TextInput ,StyleSheet,TouchableOpacity} from 'react-native';
+import CheckBox from 'react-native-checkbox';
+
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+
+import {
+  Kaede,
+  Hoshi,
+  Jiro,
+  Isao,
+  Madoka,
+  Akira,
+  Hideo,
+  Kohana,
+  Makiko,
+  Sae,
+  Fumi,
+} from 'react-native-textinput-effects';
+
+class MyReactNativeForm extends Component {
+    constructor(props){
+        super(props)
+    }
+
+  handleViewRef = ref => this.view = ref;
+
+  flipOutX = () => this.view.flipOutX(600)
+
+  animsOn = () => {
+      this.flipOutX();
+      this.props.logAnim();
+  }
+render(){
+    return(
+        <Animatable.View ref = {this.handleViewRef}>
+            <Formik
+              onSubmit={(values, actions) => {
+                        // if(values){
+                        //     alert(JSON.stringify(values))
+                        // }
+                      }}
+
+              render={(props) =>(
+                  <View style={styles.loginBox}>
+                      <View style={styles.inputBox}>
+                          <Kohana
+                            style={{ backgroundColor: '#f9f5ed' }}
+                            label={'Username'}
+                            iconClass={FontAwesomeIcon}
+                            iconName={'user-o'}
+                            iconColor={'#91627b'}
+                            iconSize={20}
+                            value={props.values.username}
+                            onChangeText={text =>props.setFieldValue('username', text)}
+                            labelStyle={{ color: '#91627b' , fontSize:15 , fontWeight:'200' }}
+                            inputStyle={{ color: '#91627b' }}
+                            useNativeDriver
+                          />
+                      </View>
+
+                      <View style={styles.inputBox}>
+
+                          <Kohana
+                            style={{ backgroundColor: '#f9f5ed' }}
+                            label={'Code'}
+                            iconClass={FontAwesomeIcon}
+                            iconName={'shield'}
+                            iconSize={20}
+                            iconColor={'#91627b'}
+                            value={props.values.password }
+                            onChangeText={text =>props.setFieldValue('password', text)}
+                            labelStyle={{ color: '#91627b' , fontSize:15 , fontWeight:'200' }}
+                            inputStyle={{ color: '#91627b' }}
+                            useNativeDriver
+                            secureTextEntry={true}
+
+                          />
+                      </View>
+                      {/* <CheckBox
+                          label='Remember me'
+                          checked={props.values.rememberMe}
+                          onChange={(checked)=>props.setFieldValue('rememberMe',!checked)}
+                          labelBefore={false}
+                          containerStyle={{margin:6,height:20,alignItems:'center',justifyContent:'center'}}
+                          labelStyle={{color:'#9EB3C2',fontSize:18}}
+                          checkboxStyle={{width:18,height:18}}
+                      /> */}
+                      <View style={{alignItems:'center'}}>
+                            <TouchableOpacity activeOpacity={0.8} onPress={this.animsOn}>
+                                <View style={styles.loginButton}>
+                                         <Text style={{color:'white',fontSize:18}}>Login</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+
+                  </View>)}
+              />
+        </Animatable.View>
+)
+}
+}
+
+
+
+
+const styles = StyleSheet.create({
+    loginBox:{
+        // flex:1,
+      margin:4,
+    //   backgroundColor:'#767F58',
+      justifyContent:'center'
+    },
+    inputBox:{
+         height:50,
+         width:280,
+        backgroundColor:'white',
+        margin:4,
+        // borderRadius:6,
+        // borderWidth:1,
+        alignSelf:'center'
+    },
+    txtInp:{
+        fontSize:16,
+        paddingLeft:10,
+        paddingTop:10,
+        flexWrap:'wrap'
+    },
+    loginButton:{
+        // height:30,
+        padding:6,
+        width:100,
+        borderRadius:6,
+        alignItems:'center',
+        justifyContent:'center',
+        marginTop:8,
+        backgroundColor:'#DD7018'
+    }
+
+});
+export default MyReactNativeForm;
+
+//
+
+
+// const LoginComponent = withFormik({
+//     // validationSchema: Yup.object().shape({
+//     //   username: Yup.string().required("First name is required."),
+//     //   password: Yup.string().required("Last name is required."),
+//     //   rememberMe: Yup.bool()
+//     // }),
+//     mapPropsToValues:({user}) => ({
+//         ...user
+//       }),
+//     validate:(values)=>{
+//         const errors={};
+//         if (!values.username) {
+//             errors.username = 'Username Required';
+//           }
+//        else if (!values.password) {
+//         errors.password = 'Password Required';
+//         }
+//
+//         if(!/^[0-9_%.+-]/.test(values.username)){
+//             errors.username="Invalid Username";
+//         }
+//         return errors;
+//     },
+//     onSubmit:({event})=>{
+//         alert("YOOOOO");
+//     },
+//     displayName:'Login Form',
+// })(LoginBox);
+//
+//
+//   {/* <View style={{alignItems:'center'}}>
+//             <TouchableOpacity activeOpacity={0.8} onPress={handleSubmit}>
+//                 <View style={styles.loginButton}>
+//                     <Text>Login</Text>
+//                 </View>
+//             </TouchableOpacity>
+//         </View> */}
+
+//
+// export default LoginComponent;
